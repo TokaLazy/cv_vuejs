2c638b329941f391e06ae5a6d10fc72255341f13
Steeve Clement <cl.steeve@gmail.com>
2017-09-19 23:35:09 +0200

    Update ^^

--- src/components/Presentation.vue
+++ src/components/Presentation.vue
@@ -14,16 +14,13 @@
         <h3>{{ data.social.title }}</h3>
         <ul>
           <li><a class="icon-github" href="https://github.com/TokaLazy" title="Regardez mes projets GitHub" target="_blank">Github</a></li>
-          <li><a class="icon-skype" href="skype:tokalazy?add" title="Rejoingez-moi sur mon Skype" target="_blank">Skype</a></li>
           <li><a class="icon-linkedin" href="https://www.linkedin.com/in/steeve-clement-a8837b91/" title="Consultez mon profil LinkedIn" target="_blank">LinkedIn</a></li>
-          <li><a class="icon-google-plus" href="https://plus.google.com/104424862838347383216" title="Suivez-moi sur mon compte Google Plus" target="_blank">Google +</a></li>
         </ul>
       </section>
       <section>
         <h3>Contactez-moi</h3>
         <ul>
           <li><a :href="'mailto:'+data.location.email">{{ data.location.email }}</a></li>
-          <li><a :href="'tel:'+data.location.phone">{{ phone }}</a></li>
         </ul>
       </section>
     </div>
